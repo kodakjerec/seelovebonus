b131c8952b1216d500eea227a27e3963f54e1870
kodak <kodakjerec@hotmail.com>
2021-03-16 17:52:03 +0800

    fix local.js bug

--- src/services/apiList/local.js
+++ src/services/apiList/local.js
@@ -4,10 +4,7 @@ import store from '@/store/index'
 let globalSettngs = store.state.globalSettings
 export default {
   local: {
-    getDropdownList: ({ type, keyword }) => {
-      if (!keyword) {
-        keyword = ''
-      }
+    getDropdownList: ({ type }) => {
       let list = globalSettngs.filter(item => { return item.Category === type })
       // 特例：ID 是數字型態時, 專用排序 (避免排成 1,10,11,2,20,21)
       switch (type) {
