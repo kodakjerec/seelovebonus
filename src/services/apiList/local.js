d2a4b8c4152391e07c196364bce93ae2bd0725d9
kodak <kodakjerec@hotmail.com>
2021-04-01 11:13:58 +0800

    fix: local.js常常遺漏資料

--- src/services/apiList/local.js
+++ src/services/apiList/local.js
@@ -1,11 +1,10 @@
 // 在前端就先篩選數值
 import store from '@/store/index'
 
-let globalSettngs = store.state.globalSettings
 export default {
   local: {
     getDropdownList: ({ type }) => {
-      let list = globalSettngs.filter(item => { return item.Category === type })
+      let list = store.state.globalSettings.filter(item => { return item.Category === type })
       // 特例：ID 是數字型態時, 專用排序 (避免排成 1,10,11,2,20,21)
       switch (type) {
         case 'City':
