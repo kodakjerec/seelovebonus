801bf076f13692c89cdad828423c5b33bcd87eac
kodak <kodakjerec@hotmail.com>
2021-04-19 10:43:09 +0800

    匯出銷貨單,未完

--- src/views/Reports/anzaRpts.vue
+++ src/views/Reports/anzaRpts.vue
@@ -2,18 +2,22 @@
   <div>
     <el-tabs v-model="activeName">
       <el-tab-pane :label="$t('__totalReport')" name="first"></el-tab-pane>
+      <el-tab-pane :label="$t('__export')+$t('__salesReport')" name="second"></el-tab-pane>
     </el-tabs>
     <total v-if="activeName==='first'"></total>
+    <wintonExport v-if="activeName==='second'"></wintonExport>
   </div>
 </template>
 
 <script>
 import total from './components/anza/glReportAnzaTotal'
+import wintonExport from './components/anza/glReportWintonExport'
 
 export default {
   name: 'AnzaRpts',
   components: {
-    total
+    total,
+    wintonExport
   },
   data () {
     return {
