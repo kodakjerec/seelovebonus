01d50780239046ca7315a099a1a82c72d11a6d91
kodak <kodakjerec@hotmail.com>
2021-05-05 17:45:44 +0800

    版號

--- src/store/index.js
+++ src/store/index.js
@@ -13,7 +13,7 @@ export default new Vuex.Store({
     userName: '',
     menuList: [],
     userProg: [],
-    version: '2021.04.13.1',
+    version: '2021.05.05.1',
     globalSettings: [],
     // 不用紀錄到localStorage
     loadingCounter: 0,
